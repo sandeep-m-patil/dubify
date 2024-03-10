5991c73c6887906f0cc3dda3e01928933036015a
Sandeep Madhavrao Patil <85793480+sandeep-m-patil@users.noreply.github.com>
2024-03-10 09:05:46 +0530

    updateed

---
+++ main.js
@@ -0,0 +1,9 @@
+function save(){
+    alert("Dubbed video is successfully saved")
+}
+function expo(){
+    alert("Dubbed video is ready to export")
+}
+function edit(){
+    alert("edit your dubbed video")
+}
