302d944e823275dc89542e8349050ad576ba7238
Sandeep Madhavrao Patil <85793480+sandeep-m-patil@users.noreply.github.com>
2024-03-10 11:55:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 function save(){
-    alert("Dubbed video is successfully saved")
+    alert("Dubbed video/text is successfully saved")
 }
 function expo(){
-    alert("Dubbed video is ready to export")
+    alert("Dubbed video/text is ready to export")
 }
 function edit(){
-    alert("edit your dubbed video")
+    alert("edit your dubbed video/text")
+}
+function video(){
+   document.querySelector('.video').style.display='block';
+   document.querySelector('.text').style.display='none';
+
+}
+function text(){
+    document.querySelector('.video').style.display='none';
+    document.querySelector('.text').style.display='block';
+ 
 }
